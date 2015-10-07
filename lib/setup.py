80c2f2788f8e0f85d87c6796ff0f08b444a3f8c7
Justin McGuire <jm@landedstar.com>
2015-10-07 14:02:04 -0400

    better dice (slowly learning markdown extensions)

---
+++ lib/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AdventureBoilerplate',
+      version='0.1',
+      description='Markdown extensions for Adventure Boilerplate',
+      author='Justin McGuire',
+      author_email='jm@landedstar.com'
+      url='https://github.org/jmcguire/adventure-boilerplate',
+      packages=['...', '...'],
+     )
+
