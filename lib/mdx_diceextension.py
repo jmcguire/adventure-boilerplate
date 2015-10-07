bc6c0a716749b868353beb94f47c0e96025675f6
Justin McGuire <jm@landedstar.com>
2015-10-07 15:22:55 -0400

    dice extension for markdown is working
     - also, ignoring .pyc files

--- lib/mdx_diceextension.py
+++ lib/mdx_diceextension.py
@@ -1,27 +1,23 @@
-from markdown.extensions import Extensions
-from markdown.inlinepatterns import Pattern, SimpleTagPattern
+from markdown.extensions import Extension
+from markdown.inlinepatterns import Pattern
 from markdown import util
-import re
 
-# look for things like 1d10, 18d4, 12d8 + 13, 10d6+1
-DICE_RE = r'(\d+d\d+(?\s*\+\s*\d+)?)'
+# look for things like 1d4, 18d6, 12d10 + 13, 10d12+1
+DICE_RE = r'\b(\d+d\d+(?:\s*\+\s*\d+)?)\b'
 
 class DiceExtension(Extension):
   """Adds dice extension to Markdown class."""
-  """Turns 1d10 to <strong class="dice">1d10</strong>."""
   def extendMarkdown(self, md, md_globals):
-    md.inlinePatterns.add('dice', DicePattern(DICE_RE, md), '<references')
-    #md.inlinePatterns.add('dice', SimpleTagPattern(DICE_RE, 'strong'), '<references')
-
+    md.inlinePatterns.add('dice', DicePattern(DICE_RE, md), '<strong')
 
 class DicePattern(Pattern):
-  """Returns a strong element, given a dice string (`1d10 + 4`)"""
+  """Returns a strong element, given a dice string (`1d10 + 4`)."""
   def handleMatch(self, m):
-    el = util.etree.Element('strong')
+    el = util.etree.Element("strong")
     el.set('class', 'dice')
     el.text = util.AtomicString(m.group(2))
     return el
 
-def makeExtension(configs=None):
-    return DiceExtension(configs=None)
+def makeExtension(configs={}):
+    return DiceExtension(configs=dict(configs))
 
