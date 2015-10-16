ed2b659482d9978ad275a929ed29a6b0b33bc5e2
Justin McGuire <jm@landedstar.com>
2015-10-15 21:32:28 -0400

    use meta data for html. built-in licensing options.
     - Use the standard meta extension to give information about the adventure, and build out the HTML from that. Just basic stuff like title, author, and license. For this, we moved some of the markdown file markup into the wrap extension, specifically the header stuff.
    
     - We want to enable people to license their adventures anyway they want. We have a license.yaml file with information about a lot of common licenses. If the author selects one of them in their meta data "License: CC0", the appropriate license information will be found and put into the adventure's footer.
    
     - The if the author doesn't select a license, it defaults to the old standard, "All Rights Reserved". This isn't even legally necessary anymore, but it's nice provide clarity.
    
     - Updated the README and TODO. We're getting closer to a proper beta.

--- README.md
+++ README.md
@@ -4,27 +4,41 @@ HTML/CSS Boilerplate (and one-click Markdown conversion) for RPG Adventures. Cur
 
 ## Description
 
-Authors should be proud of how their adventures look. This project gives that to them. Using simple HTML, or even simpler Markdown, adventure authors will be able to produce a great-looking adventure.
+Authors should be proud of how their adventures look. This project gives that to them. Using simple HTML, or even simpler Markdown, adventure authors will be able to produce a great-looking adventure, suitable for web viewing or printing.
 
-This project is really two project mooshed together.
+This project is really two projects mooshed together.
 
 **The first part is HTML/CSS boilerplate for Dungeons and Dragons adventures.** Inspired by the HTML5 boierplate project, this aims to be an easy-to-use CSS file and HTML standard that anyone can use to markup their adventure with.
 
-The result will be a readable and typography-designed webpage. It will work well on screens, tablets, mobile devices, and will even print beautifully.
+The result will be a professional webpage. It will work well on screens, tablets, mobile devices, and will even print beautifully.
 
 **The second part is an adventure markdown converter.** People who don't know HTML should still be able to build a nice looking adventure.
 
 We're accomplishing that by using the simple and increasingly-common Markdown language. Authors will be able to write a markdown document, then run a simple build script called "build.sh", and an entire HTML website will be produced for them.
 
-We're using Python markdown, with some custom extensions to make a lot of magic happen.
+We're using Python markdown, with some custom extensions to make a lot of magic happen without the author worrying about it. The goal is to default to a great looking adventure.
 
 ## Installation
 
-Download, write an adventure using markdown. Then run the build.sh command.
+Download this software.
+
+Install a couple python packages, if you don't already have them:
+
+  $ pip install markdown
+  $ pip install pyyaml
+
+If you're on Mac, which this program was developed on, use this:
+
+  $ brew install libyaml
+  $ sudo python -m easy_install pyyaml
 
 ## Usage
 
-Run the build.sh command. Further documentation coming when the build.sh command is in build.
+Write an adventure using markdown.
+
+Run the build.sh command.
+
+Further documentation coming when the build.sh command is in build.
 
 ## Credits
 
--- lib/mdx_wrap.py
+++ lib/mdx_wrap.py
@@ -5,6 +5,8 @@ Wrap Markdown Extension for Adventure Boilerplate
 """
 
 import markdown
+import yaml
+from datetime import datetime
 
 class WrapExtension(markdown.Extension):
 
@@ -24,7 +26,7 @@ class WrapProcessor(markdown.postprocessors.Postprocessor):
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>{title}</title>
+    <title>{title} - {author}</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -55,25 +57,51 @@ class WrapProcessor(markdown.postprocessors.Postprocessor):
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 
     <script src="https://use.typekit.net/kff4qkp.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <script>try{{Typekit.load({{ async: true }});}}catch(e){{}}</script>
   </head>
   <body>
     <!--[if lt IE 8]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
+    <div id="parallax">
+      <div class="text-vcenter">
+        <h1>{title}<span>{subtitle}</span></h1>
+        <p class="byline">by {author}</p>
+      </div>
+    </div>
+    <main>
 """
 
       bottom_part="""
-<footer>
-  AUTHOR INFO
-  AUTHOR CHOOSEN LICENSE
-  <p>Formatting courtesy of <a href="http://github.com/jmcguire/adventure-boilerplate">Adventure Boilerplate</a> &copy; 2015 Justin McGuire with the <a href="https://opensource.org/licenses/MIT">MIT license</a>.</p>
-</footer>
-</body>
+    </main>
+    <footer>
+      <p>{author}</p>
+      <p>{license_html}</p>
+      <p>Formatting courtesy of <a href="http://github.com/jmcguire/adventure-boilerplate">Adventure Boilerplate</a> &copy; 2015 Justin McGuire with the <a href="https://opensource.org/licenses/MIT">MIT license</a>.</p>
+    </footer>
+  </body>
 </html>
 """
 
-      return (top_part.format(title=self.markdown.advboil_wrap_opts['title']) + text + bottom_part)
+      meta = self.markdown.Meta
+      # set good-enough defaults
+      meta.setdefault('title', ['A D&amp;D5 Adventure'])
+      meta.setdefault('author', ['Anonymous'])
+      meta.setdefault('subtitle', [''])
+      meta.setdefault('license', ['None'])
+
+      ## create the license html snippet
+      licenses=yaml.load(file('licenses.yaml'))
+      license_code = meta['license'][0].upper()
+      if license_code == 'None':
+        license_html='License: &copy; %d %s' % (datetime.now().year, meta['author'][0])
+      elif license_code in licenses:
+        license_html='<p>License: <a href="%s">%s</a></p>' % ( licenses[license_code]['url'], licenses[license_code]['title'] )
+      else:
+        license_html='License: %s' % meta['license'][0]
+
+      html = top_part + text + bottom_part
+      return (html.format(title=meta['title'][0], author=meta['author'][0], subtitle=meta['subtitle'][0], license_html=license_html))
 
 def makeExtension(configs=None):
     return WrapExtension(configs=configs)
